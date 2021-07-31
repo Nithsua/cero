6428336be4b8aac5ca672697a5f8bde050dbd44a
Nivas Muthu M G <nivasmuthu452@gmail.com>
2021-07-31 12:23:00 +0530

    new sub module password added to module password

---
+++ src/password/password.rs
@@ -0,0 +1,38 @@
+pub struct Passwords {
+    passwords: Vec<Password>,
+}
+
+impl Passwords {
+    pub fn new(passwords: Vec<Password>) -> Self {
+        Passwords {
+            passwords: passwords,
+        }
+    }
+
+    pub fn get_passwords(&self) -> &Vec<Password> {
+        &self.passwords
+    }
+
+    pub fn length_of_passwords(&self) -> usize {
+        self.passwords.len()
+    }
+}
+
+#[derive(Debug)]
+pub struct Password {
+    pub id: String,
+    pub url: String,
+    pub username: String,
+    pub password: String,
+}
+
+impl Password {
+    pub fn new(url: String, username: String, password: String) -> Self {
+        Password {
+            id: "  ".to_string(),
+            url: url,
+            username: username,
+            password: password,
+        }
+    }
+}
